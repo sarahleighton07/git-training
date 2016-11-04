a164490f702785a0bbae36b15c5e088f05147141
sleighton <sleighton@companieshouse.gov.uk>
2016-11-04 11:26:00 +0000

    change hello world text

--- Hello_world.java
+++ Hello_world.java
@@ -1,6 +1,8 @@
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("I Love Conflicts!");
+public static void main(String[] args) {
+        System.out.println("This is so much fun!");
     }
 }
